e9260fbcc9d44ad059ca3f90085ae067cb6334e2
sineadccheung <sineadccheung@gmail.com>
2016-12-25 13:04:51 +0800

    assignment 23 completed

--- bloc-jams/script/landing.js
+++ bloc-jams/script/landing.js
@@ -1,16 +1,19 @@
 var pointsArray = document.getElementsByClassName('point');
 
+var revealPoint = function (point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+  };
+
 var animatePoints = function(points) {
 
-  var revealPoint = function (index) {
-    points[index].style.opacity = 1;
-    points[index].style.transform = "scaleX(1) translateY(0)";
-    points[index].style.msTransform = "scaleX(1) translateY(0)";
-    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-  }
+  forEach(points, revealPoint);
 
   for (var i=0; i<points.length; i++){
       revealPoint(i);
+
   }
 
 };
@@ -25,7 +28,7 @@ var animatePoints = function(points) {
    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
    window.addEventListener("scroll", function(event) {
-          
+
           if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
               animatePoints(pointsArray);
           }
