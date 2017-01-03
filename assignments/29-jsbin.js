e7844f11e1d59f738f4fa74bd608a17353492e64
sineadccheung <sineadccheung@gmail.com>
2017-01-03 21:26:08 +0800

    assignment 29 completed

--- assignments/29-jsbin.js
+++ assignments/29-jsbin.js
@@ -1,31 +1,17 @@
-// window.onload = function() {
-//   alert("Pick a healthy snack from the refrigerator!");
-// };
-
-$(window).ready(function(){
+$(document).ready(function() {
   alert("Pick a healthy snack from the refrigerator!");
 });
 
-// var oranges = document.getElementsByClassName("oranges");
+var $oranges = $('.oranges');
 
-$('oranges').click(function(){
-  border: 5px, solid, orange;
+$oranges.click(function() {
+  $(this).css('border', '5px solid orange');
   alert("You chose an orange!");
 });
 
-// oranges[0].onclick = function() {
-//   oranges[0].style.border = "5px solid orange";
-//   alert("You chose an orange!");
-// };
+var $apples = $('.apples');
 
-// var apples = document.getElementsByClassName("apples");
-
-// apples[0].onclick = function() {
-//   apples[0].style.border = "5px solid red";
-//   alert("You chose an apple!");
-// };
-
-$('apples').click(function(){
-  border: 5px, solid, red;
+$apples.click(function(){
+  $(this).css('border', '5px solid red');
   alert("You chose an apple!");
 });
